76521b53060d45a0809784e141d3dcba61ce020f
Ningaanza Remy <remimaradona@gmail.com>
2020-05-15 16:33:24 -0400

    created Redux actions:Add,Remove,open,close,open myList and close myList

---
+++ assets/js/actions/allAction.js
@@ -0,0 +1,36 @@
+export const addingBook = book =>{
+  return {
+    type: 'ADD_BOOK',
+    payload: book
+  };
+};
+
+export const removingBook = book =>{
+  return{
+    type: 'REMOVE_BOOK',
+    payload: book
+  };
+};
+
+export const openingInfoBook = book =>{
+  return{
+    type: 'OPEN_BOOK',
+    payload: book
+  };
+};
+export const closingInfoBook = book =>{
+  return{
+    type: 'CLOSE_OPEN_BOOK',
+    payload: book
+  };
+};
+export const openingMylist = () =>{
+  return{
+    type: 'OPEN_MY_LIST',
+  };
+};
+export const closingngMylist = () =>{
+  return{
+    type: 'OPEN_MY_LIST',
+  };
+};
